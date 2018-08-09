0117cec449687f3a08fe16d9e2834dcea6dfecb6
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-08-09 21:33:12 +0430

    Add page2

---
+++ pages/page2.js
@@ -0,0 +1,34 @@
+import withData from '../lib/apollo'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const LinkList = () => (
+  <Query
+    pollInterval={1500}
+    query={gql`
+      {
+        allLinks {
+          id
+          description
+          url
+        }
+      }
+    `}
+  >
+    {({ loading, error, data, startPolling, stopPolling }) => {
+      if (loading) return <p>Loading...</p>
+      if (error) return <p>Error :(</p>
+
+      return data.allLinks.map(({ id, url, description }) => (
+        <div key={id}>
+          <p>
+            {`${id}: (${description})`}
+            <a href={url}>Link</a>
+          </p>
+        </div>
+      ))
+    }}
+  </Query>
+)
+
+export default withData(LinkList)
